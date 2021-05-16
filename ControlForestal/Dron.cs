2ac7f42e7484b60b811b36bb6e990bb9211cd092
Marcos Alvarez Varela <marcos.av@outlook.es>
2021-05-16 14:27:43 +0200

    Agregar archivos de proyecto.

---
+++ ControlForestal/Dron.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControlForestal
+{
+    /// <summary>
+    /// Representa un dron para ser programado mediante unas coordenadas X Y ademas de su orientacion.
+    /// </summary>
+    class Dron
+    {
+        private int PosicionX { get; set; }
+        private int PosicionY { get; set; }
+        private char Orientacion { get; set; }
+
+    }
+}

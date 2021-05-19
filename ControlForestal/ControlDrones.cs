047194b919a47145d1aaf97fb42dcec6317199cb
Marcos Alvarez Varela <marcos.av@outlook.es>
2021-05-19 22:30:53 +0200

    Mejoras de codigo y nombre de clase cambiado para mayor exactitud con la entidad que representa

--- ControlForestal/ControlDrones.cs
+++ ControlForestal/ControlDrones.cs
@@ -1,26 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace ControlForestal
 {
     /// <summary>
-    /// Representa un parque forestal sobre el que se define un area a vigilar asi como los drones que se programan para ejecutar la vigilancia.
+    /// Representa el contol de drones sobre el que se define el area a vigilar asi como los drones que se programan para ejecutar la vigilancia.
     /// </summary>
-    public class ParqueForestal
+    public class ControlDrones
     {
         /// <summary>
         /// Anchura del area a vigilar.
         /// </summary>
         private int LongitudX { get; set; }
+
         /// <summary>
         /// Altura del area a vigilar.
         /// </summary>
         private int LongitudY { get; set; }
 
+        /// <summary>
+        /// Nombre del parque sobre el que se implementara el area a vigilar.
+        /// </summary>
         private string Nombre { get; set; }
-        public ParqueForestal(string nombreParque)
+
+        public ControlDrones(string nombreParque)
         {
             Nombre = nombreParque;
         }
@@ -36,6 +40,7 @@ namespace ControlForestal
             Console.WriteLine("3-Introduzca la secuencia de ordenes que debera realizar el dron segudias(Ej: LRM). \n");
             Console.WriteLine("4-Para dar la orden a los drones de que inicien la patrulla pulse intro sin introducir ningun dato despues de haber programado al menos un dron \n");
             Console.WriteLine("Puede introducir tantos drones como desee siguiendo los pasos 2 y 3 de forma continuada");
+
             DefinirArea();
         }
 
@@ -57,11 +62,11 @@ namespace ControlForestal
                 LongitudY = areaY;
                 AñadirDron();
             }
+            else
             {
                 Console.WriteLine("ERROR en el area. Introduzca valores correctos, solo se permiten dos numeros enteros mayores que 0");
                 DefinirArea();
             }
-
         }
 
         /// <summary>
@@ -83,8 +88,8 @@ namespace ControlForestal
             ///Comprobamos que los datos introducidos para indicar la posicion inicial del dron son validos.
             bool datosValidos = posicionInicial.Count() == 3 && int.TryParse(posicionInicial[0], out Xinicial) && int.TryParse(posicionInicial[1], out Yinicial)
                 && char.TryParse(posicionInicial[2], out orientacionInicial) && posicionInicial[2].ToUpper().IndexOfAny(puntosCardinales) != -1;
-     
-            if(datosValidos && !(Xinicial < LongitudX || Xinicial > LongitudX) && !(Yinicial < LongitudY || Yinicial > LongitudY))
+
+            if (datosValidos && !(Xinicial < LongitudX || Xinicial > LongitudX) && !(Yinicial < LongitudY || Yinicial > LongitudY))
             {
                 Dron dron = new Dron(Xinicial, Yinicial, orientacionInicial);
                 ProgramarRutaDron(dron);
@@ -94,18 +99,13 @@ namespace ControlForestal
                 Console.WriteLine("ERROR Coordenadas de incio erroneas");
                 AñadirDron();
             }
-
-            Console.WriteLine("Exito");
-            AñadirDron();
         }
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         private void ProgramarRutaDron(Dron dron)
         {
-
         }
-
     }
 }
